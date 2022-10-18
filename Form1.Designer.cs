ba89a28ae2be30b4394189a48be2840e5a28f6a5
Alumno <Alumno@PC-31>
2022-10-18 12:30:09 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Reproductor_multimedia
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lb_lista_archivos = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.mpMediaPlayer = new AxWMPLib.AxWindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.mpMediaPlayer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lb_lista_archivos
+            // 
+            this.lb_lista_archivos.FormattingEnabled = true;
+            this.lb_lista_archivos.Location = new System.Drawing.Point(368, 12);
+            this.lb_lista_archivos.Name = "lb_lista_archivos";
+            this.lb_lista_archivos.Size = new System.Drawing.Size(184, 251);
+            this.lb_lista_archivos.TabIndex = 1;
+            this.lb_lista_archivos.SelectedIndexChanged += new System.EventHandler(this.lb_lista_archivos_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(429, 276);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "&Abrir";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // mpMediaPlayer
+            // 
+            this.mpMediaPlayer.Enabled = true;
+            this.mpMediaPlayer.Location = new System.Drawing.Point(0, -1);
+            this.mpMediaPlayer.Name = "mpMediaPlayer";
+            this.mpMediaPlayer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("mpMediaPlayer.OcxState")));
+            this.mpMediaPlayer.Size = new System.Drawing.Size(362, 312);
+            this.mpMediaPlayer.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(564, 311);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lb_lista_archivos);
+            this.Controls.Add(this.mpMediaPlayer);
+            this.ForeColor = System.Drawing.SystemColors.Info;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.mpMediaPlayer)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer mpMediaPlayer;
+        private System.Windows.Forms.ListBox lb_lista_archivos;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Reproductor_multimedia
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lb_lista_archivos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            MediaFile file = lb_lista_archivos.SelectedItem as MediaFile;
+            if(file != null)
+            {
+                mpMediaPlayer.URL = file.Path;
+                mpMediaPlayer.Ctlcontrols.play();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using(OpenFileDialog ofd = new OpenFileDialog() { Multiselect = true, ValidateNames = true, Filter = "WMV|*.wmv|WAV|*.wav|MP3|*.mp3|MP4|*.mp4|MKV|*.mkv" })
+            {
+                if(ofd.ShowDialog() == DialogResult.OK)
+                {
+                    List<MediaFile> file = new List<MediaFile>();
+                    foreach(string FileName in ofd.FileNames)
+                    {
+                        FileInfo fi = new FileInfo(FileName);
+                        file.Add(new MediaFile() { FileName = Path.GetFileNameWithoutExtension(fi.FullName), Path = fi.FullName });
+                    }
+                    lb_lista_archivos.DataSource = file;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lb_lista_archivos.ValueMember = "Path";
+            lb_lista_archivos.DisplayMember = "FileName";
+        }
+    }
+}
